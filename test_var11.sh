6b6a031ca08fc1fd080da3d24f654e4e83c0570c
AngryReiser <halk001000@gmail.com>
2017-10-01 22:34:55 +0300

    INIT

---
+++ test_var11.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# file: examples/equality_test.sh
+
+testEquality()
+{
+	sh 11.sh
+	actual=`cat actual.tsv`
+	expected=`cat expected.tsv`
+  assertEquals "$actual" "$expected"
+}
+
+# load shunit2
+. shunit2-2.1.6/src/shunit2
